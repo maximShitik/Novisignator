0d459331dce4ed45f5b94a275dbe33400dc51624
maximShitik <maximshitik29@gmail.com>
2026-04-13 20:30:37 +0300

    chunk 1.3 done

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from routes.chat import chat_bp
-from routes.coupon import coupon_bp
+from routes.coupon import create_coupon_routes
 from routes.navigation import navigation_bp
 from routes.session import session_bp
 from routes.admin import admin_bp
+
+from infrastructure.kafka import create_kafka_producer
+from infrastructure.db import create_db_pool
+from repositories.coupon_repository import CouponRepository
+from services.coupon_service import CouponService
+
+DATABASE_URL = "postgresql://user:password@localhost/malldb"
+KAFKA_SERVERS = "localhost:9092"
+
+kafka_producer = create_kafka_producer(KAFKA_SERVERS)
+db_pool = create_db_pool(DATABASE_URL)
+
+
+coupon_repo = CouponRepository(db_pool)
+coupon_service = CouponService(coupon_repo, kafka_producer)
+coupon_bp = create_coupon_routes(coupon_service)
 
 app = Flask(__name__)
 
--- backend/routes/coupon.py
+++ backend/routes/coupon.py
@@ -1,11 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify,request
 
-coupon_bp = Blueprint('coupon', __name__)
 
-@coupon_bp.route('/coupon/claim', methods=['POST'])
-def claim_coupon():
-    pass
+def create_coupon_routes(coupon_service):
+    coupon_bp = Blueprint('coupon', __name__)
 
-@coupon_bp.route('/coupon', methods=['GET'])
-def get_coupon():
-    pass
+    @coupon_bp.route('/coupon/<store_id>', methods=['GET'])
+    def display_coupons(store_id):
+        result = coupon_service.display_coupons(store_id)
+        return jsonify(result)
+
+    
+
+    @coupon_bp.route('/coupon/claim', methods=['POST'])
+    def claim_coupon():
+        data = request.get_json()
+        coupon_code = data['coupon_code']
+        result = coupon_service.claim_coupon(coupon_code)
+        return jsonify(result)
+    
+    return coupon_bp
